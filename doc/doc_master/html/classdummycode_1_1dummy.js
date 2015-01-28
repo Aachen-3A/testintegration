8a6037e6bf72c21dd427c8f7cbd74ea79e89e9b3
Tobias Pook <pook@physik.rwth-aachen.de>
2015-01-28 14:36:35 +0100

    updated documentation

--- doc/doc_master/html/classdummycode_1_1dummy.js
+++ doc/doc_master/html/classdummycode_1_1dummy.js
@@ -5,12 +5,12 @@ var classdummycode_1_1dummy =
     [ "aa", "classdummycode_1_1dummy.html#ade8c2a23728fea407400de565af9d924", null ],
     [ "bb", "classdummycode_1_1dummy.html#a8fc4a473d7dc65b38abc8ca2bbf56d0c", null ],
     [ "dd", "classdummycode_1_1dummy.html#af2feb69e325549343d505d0f5afaacd0", null ],
-    [ "kj", "classdummycode_1_1dummy.html#acd218190c56cf719aff4b645ee816f48", null ],
     [ "ll", "classdummycode_1_1dummy.html#aff4159330a4c573597b401b5503818d7", null ],
     [ "oo", "classdummycode_1_1dummy.html#a49635a45286e102fb960d0bc4f4c3621", null ],
     [ "pp", "classdummycode_1_1dummy.html#a4ded8e4fa38d817b96c7b21a869a16ae", null ],
     [ "tt", "classdummycode_1_1dummy.html#ac1a6206c9bd102d051018258c3b54de9", null ],
     [ "ww", "classdummycode_1_1dummy.html#a65b1604eb43ffa41eb49a02655397fb7", null ],
     [ "xx", "classdummycode_1_1dummy.html#af1ae41fb24a1c0a52ae60bd1f2db9f5f", null ],
-    [ "yy", "classdummycode_1_1dummy.html#ad0f056c4695b2525f883dbe1004fa7ba", null ]
+    [ "yy", "classdummycode_1_1dummy.html#ad0f056c4695b2525f883dbe1004fa7ba", null ],
+    [ "zz", "classdummycode_1_1dummy.html#af5c829ccdb5a89e8ba0f2cf476cb2aa0", null ]
 ];
--- dummycode.py
+++ dummycode.py
@@ -10,7 +10,7 @@ class dummy():
     self.ww = 2
     self.oo = 2
     self.tt = 2
-    self.kj = 2
+    self.zz = 2
   ## some info
   #
   # @param self pointer
