43de317b4c7038fbde9555f7afea37c18ac3bbf6
Tobias Pook <pook@physik.rwth-aachen.de>
2015-01-29 15:27:39 +0100

    updated doxygen documentation for branch: testbranch

--- doc/doc_testbranch/html/annotated.js
+++ doc/doc_testbranch/html/annotated.js
@@ -1,4 +1,8 @@
 var annotated =
 [
-    [ "dummycode", "namespacedummycode.html", "namespacedummycode" ]
+    [ "dummycode", "namespacedummycode.html", "namespacedummycode" ],
+    [ "githookcontroller", "namespacegithookcontroller.html", "namespacegithookcontroller" ],
+    [ "post-commit", "namespacepost-commit.html", null ],
+    [ "pre-commit", "namespacepre-commit.html", null ],
+    [ "pre-push", "namespacepre-push.html", null ]
 ];
